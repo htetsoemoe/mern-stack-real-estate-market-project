e1d026f4d18ec8e89f027be2c7a99bce5cd562b7
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-12-05 11:03:00 +0630

    Header ui component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,20 @@ import Signin from './pages/Signin'
 import SignOut from './pages/SignOut'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/sign-in' element={<Signin />} />
-      <Route path='/sign-out' element={<SignOut />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/profile' element={<Profile />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </>
   )
 }
 
